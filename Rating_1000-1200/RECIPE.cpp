dc32aea68a70219b0b3d5b117881583f14594d47
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-02-13 12:16:08 +0530

    init

---
+++ Rating_1000-1200/RECIPE.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+     int T;
+     int size;
+     cin >> T;
+
+     while (T--)
+     {
+          // int min = INT_MIN;
+          cin >> size;
+          int arr[size];
+          for (long long int i = 0; i < size; i++)
+          {
+               cin >> arr[i];
+               cout<<arr[i]<<endl;
+          }
+
+          long long int k = 1;
+     }
+
+     return 0;
+}
