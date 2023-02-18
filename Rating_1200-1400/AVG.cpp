3d83ae1119cc40fc8a0cc4e56be2659a70f4100d
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-02-18 23:59:40 +0530

    init

---
+++ Rating_1200-1400/AVG.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--)
+	{
+	    int n, k, v, s=0, b=0;
+	    cin>>n>>k>>v;
+	    int a[n];
+	    for(int i=0; i<n; i++)
+	    {
+	        cin>>a[i];
+	        s=s+a[i];
+	    }
+	    b=v*(n+k)-s;
+	    if(b>0 && ((b%k)==0)) cout<<b/k<<endl;
+	    else cout<<-1<<endl;
+	}
+	
+	return 0;
+}
