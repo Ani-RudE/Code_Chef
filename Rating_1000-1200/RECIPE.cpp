2b643d6bdfc35c96a1af68e1eadf168c90238385
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-02-23 21:18:49 +0530

    init

--- Rating_1000-1200/RECIPE.cpp
+++ Rating_1000-1200/RECIPE.cpp
@@ -1,25 +1,27 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
 int main()
 {
-     int T;
-     int size;
-     cin >> T;
-
-     while (T--)
-     {
-          // int min = INT_MIN;
-          cin >> size;
-          int arr[size];
-          for (long long int i = 0; i < size; i++)
-          {
-               cin >> arr[i];
-               cout<<arr[i]<<endl;
-          }
-
-          long long int k = 1;
-     }
-
-     return 0;
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int n;  cin>>n;
+        int a[n];
+        
+        for(int i=0; i<n; i++)  cin>>a[i];
+        
+        // finding gcd of the given array
+        int r = a[0];
+        for(int i=1; i<n; i++)  r = __gcd(r, a[i]);
+        
+        for(int i=0; i<n; i++)  cout<<a[i]/r<<" ";
+        
+        cout<<endl;
+        
+    }
+	
+	return 0;
 }
