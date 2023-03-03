c06b01ebe950c11f2ae75f05af1a272c9ecca537
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-03-03 11:16:04 +0530

    init

--- Rating_1000-1200/APPLEORANGE.cpp
+++ Rating_1000-1200/APPLEORANGE.cpp
@@ -1,20 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int gcd(int a, int b)
-{
-	int x=min(a, b);
-	while (x>0)
-	{
-		if (a%x==0 && b%x==0)
-		    break;
-		
-		x--;
-	}
-	
-	return x;
-}
-
 int main()
 {
     int T;
@@ -25,7 +11,16 @@ int main()
         int N, M;
         cin>>N>>M;
         
-        cout<<gcd(N,M)<<endl;
+        // int x=min(N, M);
+        // while (x>0)
+        // {
+        //     if (N%x==0 && M%x==0)
+        //         break;
+            
+        //     x--;
+        // }
+        
+        cout<<__gcd(N,M)<<endl;
     }
 
     return 0;
