1a8c6e0df6d1f7d3a88442b01a4f25a2696b51b1
Aniruddha Dewangan <ani.dewangan2002@gmail.com>
2023-03-08 03:54:00 +0530

    init

---
+++ Rating_0-1000/EASYPERM.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int T;
+    cin>>T;
+
+    while(T--)
+    {
+        long long int N;
+        cin>>N;
+        long long int a[N];
+        long long x=0;
+        
+        for (long long int i=0; i<N; i++)
+        {
+            cout<<N-i<<" ";
+        }
+        
+        cout<<endl;
+    }
+
+    return 0;
+}
